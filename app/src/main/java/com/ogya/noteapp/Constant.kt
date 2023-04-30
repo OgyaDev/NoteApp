fa0014c95aa279f484c72cbb8f82391cf184ca3e
OguzDev <oguzhanyrdmcDev@gmail.com>
2023-04-30 08:52:04 +0300

    pass argument another activity

---
+++ app/src/main/java/com/ogya/noteapp/Constant.kt
@@ -0,0 +1,5 @@
+package com.ogya.noteapp
+
+object IntentConstants{
+    const val EXTRA_NOTE_POSITION = "EXTRA_NOTE_POSITION"
+}
--- app/src/main/java/com/ogya/noteapp/view/NoteListActivity.kt
+++ app/src/main/java/com/ogya/noteapp/view/NoteListActivity.kt
@@ -2,6 +2,7 @@ package com.ogya.noteapp.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.ogya.noteapp.IntentConstants
 import com.ogya.noteapp.R
 import com.ogya.noteapp.databinding.ActivityNoteListBinding
 import com.ogya.noteapp.datamanager.DataManager
@@ -33,5 +35,11 @@ class NoteListActivity : AppCompatActivity() {
         binding.contentNoteList.noteList.adapter = ArrayAdapter(this,
             android.R.layout.simple_list_item_1,
             DataManager.notes)
+
+        binding.contentNoteList.noteList.setOnItemClickListener { parant, view, position, id ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            activityIntent.putExtra(IntentConstants.EXTRA_NOTE_POSITION, position)
+            startActivity(activityIntent)
+        }
     }
 }
